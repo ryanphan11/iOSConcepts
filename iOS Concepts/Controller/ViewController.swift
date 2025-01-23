c7fd25f6701d09992874dfcc53b41eeaed5c4bfb
Ryan Phan <ryanphan11@gmail.com>
2025-01-23 09:48:40 -0800

    Update project structure

--- iOS Concepts/Controller/ViewController.swift
+++ iOS Concepts/Controller/ViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  MainViewController.swift
 //  iOS Concepts
 //
 //  Created by Ryan Phan on 1/17/25.
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class MainViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
